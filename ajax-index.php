6d8f521b5217bc9806e812f6f09458ec72686251
lililala0112 <lililala0112@gmail.com>
2016-09-26 15:04:26 +0800

    ajax-build

---
+++ ajax-index.php
@@ -0,0 +1,126 @@
+<?php 
+   /**
+   * Ajax url
+   */
+   header("Content-Type:text/html; charset=utf-8");
+   class useAPI{
+
+     function __construct(){
+          $db_host = "localhost";
+          $db_user = "root";
+          $db_pwd = "1234";
+          $seldb = "snewup_db";
+          
+          $this->mysqli = new mysqli( $db_host,$db_user,$db_pwd, $seldb);
+
+          /* check connection */
+          if ($this->mysqli->connect_error) {
+              printf("Connect failed: %s\n", mysqli_connect_error());
+              exit();
+          }
+
+          $this->mysqli->set_charset("utf8");
+     }
+
+     function err_log($log){
+        return htmlspecialchars("錯誤訊息:".$log);
+     }
+
+
+
+     var $action;
+     var $DBname;
+     var $sql;
+     var $resdata;
+
+     function setData($sql){
+        $this->sql = $sql;
+     }
+
+     function showData()
+     {
+
+      $sql=$this->sql;
+      $db_result = $this->mysqli->query($sql);
+      $count = $db_result->num_rows;//筆數
+      try{
+          $resdata = mysqli_fetch_all($db_result,MYSQLI_ASSOC);
+          $return = array(
+                 'status' => 1,
+                 'resdata'=> $resdata,
+                 'count'=> $count
+          );
+      }catch(mysqli_sql_exception $e){
+        throw new MySQLiQueryException($db_result, $e->getMessage(), $e->getCode());
+        return $this->Result;
+          $error = $this->err_log($e->getMessage());
+          $return = array(
+                 'status' => 0,
+                 'error'=> $error
+          );
+      }
+      echo json_encode($return);
+
+     }
+
+     function insertData(){
+        $sql=$this->sql;
+        $msg = '感謝您的填寫<br/>表單已成功送出!!';
+        try{
+            $this->mysqli->query($sql);
+            $id = $this->mysqli->insert_id;
+            $resdata = $_POST;
+            $return = array(
+                   'status' => 1,
+                   'resdata'=> $resdata,
+                   'msg'=> $msg,
+                   'id'=> $id
+            );
+        }catch(mysqli_sql_exception $e){
+          throw new MySQLiQueryException($db_result, $e->getMessage(), $e->getCode());
+          return $this->Result;
+            $error = $this->err_log($e->getMessage());
+            $return = array(
+                   'status' => 0,
+                   'error'=> $error
+            );
+        }
+        echo json_encode($return);
+     }
+     function checkData(){
+        $sql=$this->sql;
+        //是否存在 1存在0不存在
+        $db_result = $this->mysqli->query($sql);
+        try{
+            $this->mysqli->query($sql);
+            $checkAccoutExist = $db_result->num_rows;
+            //判斷是否存在
+              $db_PassWord = mysqli_fetch_object($db_result)->password;
+            function checkPw(){
+               return $db_PassWord;
+               echo $db_PassWord;
+            }
+            $checkPw = checkPw();
+            $return = array(
+                   'status'=>1,
+                   'checkExist'=>$checkAccoutExist,
+                   'checkPw'=>$checkPw
+            );
+        }catch(mysqli_sql_exception $e){
+          throw new MySQLiQueryException($db_result, $e->getMessage(), $e->getCode());
+          return $this->Result;
+            $error = $this->err_log($e->getMessage());
+            $return = array(
+                   'status' => 0,
+                   'error'=> $error
+            );
+        }
+        echo json_encode($return);
+     }
+
+
+     function __destruct(){
+        $this->mysqli->close();
+     }
+   }
+ ?>
--- lib/useAPI.php
+++ lib/useAPI.php
@@ -1,7 +1,9 @@
 <?php 
+
+    
     header("Content-Type:text/html; charset=utf-8");
     include("connMysql.php");
-
+    include("../ajax-index.php");
     /*
     *參照教學:https://gist.github.com/jonsuh/3739844
     */
@@ -9,7 +11,15 @@
       if (isset($_POST["action"]) && !empty($_POST["action"])) { //Checks if action value exists
         $action = $_POST["action"];
         switch($action) { //Switch case for value of action
-          case "inqueryForm": insertContactDB(); break;
+          case "inqueryForm": 
+             insertContactDB();
+             break;
+          case "inquerySheet": 
+             inquerySheet();
+             break;
+          case "checkAccout": 
+             checkUserAccout(); 
+             break;
         }
       }
     }
@@ -18,33 +28,62 @@
       return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
     }
     function insertContactDB(){
-            
-        //新增一筆至資料庫
-            global $mysqli;
-            $sql_query = "SELECT * FROM `contact`";
-            $db_result = $mysqli->query($sql_query);
-            if(!$db_result) die("資料表連結失敗");     
-           
-            $name = $mysqli->real_escape_string($_POST['name']);
-            $company = $mysqli->real_escape_string($_POST['company']);
-            $phone = $mysqli->real_escape_string($_POST['phone']);
-            $mail = $mysqli->real_escape_string($_POST['mail']);
-            $comment = $mysqli->real_escape_string($_POST['comment']);
-            $sql = "INSERT INTO `contact`(`ID`, `Name`, `Company`, `Phone`, `Mail`, `Date`, `Comment`) VALUES (NULL,'{$name}','{$company}','{$phone}','{$mail}',NOW(),'{$comment}')";
-            $mysqli->query($sql) or die($mysqli->connect_error);
-           $insertId = $mysqli->insert_id;
-           $getResult = $_POST;
-           $return = array(
-                  'status' => 1,
-                  'message'=> '表單已送出<br/>感謝您的填寫!',
-                  'resdata'=> $getResult
-           );
-           echo json_encode($return);
-           
+         echo 'insertCountactDB';   
+         $ajaxUrl = new useAPI;
+         $name = real_escape_string($_POST['name']);
+         $company = real_escape_string($_POST['company']);
+         $phone = real_escape_string($_POST['phone']);
+         $mail = real_escape_string($_POST['mail']);
+         $comment = real_escape_string($_POST['comment']);
+         $sql = "INSERT INTO `contact`(`ID`, `Name`, `Company`, `Phone`, `Mail`, `Date`, `Comment`) VALUES (NULL,'{$name}','{$company}','{$phone}','{$mail}',NOW(),'{$comment}')";
+         $resdata = $_POST;
+         $resdata['msg']="感謝您的填寫<br/>表單已成功送出!!";
+         $ajaxUrl->setData($sql);//寫入
+         $ajaxUrl->insertData();//回傳
+   }
 
 
- 
+   function inquerySheet(){
+         $ajaxUrl = new useAPI;
+         $sql = "SELECT * FROM `contac`";
+         $ajaxUrl->setData($sql);//寫入
+         $ajaxUrl->showData();//回傳
+   }
 
+
+   function checkUserAccout(){
+    
+        //查詢資料庫是否存在
+         if( !empty($_POST['username']) && !empty($_POST['password'])){
+
+           $post_username = $_POST['username'];
+           $post_password = crypt ($_POST['password']);
+           echo $post_username.$post_password;
+
+           //查詢資料庫是否存在
+               global $mysqli;
+               $sqli= "SELECT * FROM `admin` where account = '{$post_username}'";
+               $db_result = $mysqli->query($sqli);
+               if(!$db_result) die("資料表連結失敗");     
+               //是否存在 1存在0不存在
+               $checkAccout = $db_result->num_rows;
+               //讀出此筆資料
+               echo $checkAccout;
+
+               if( $checkAccout == 0){
+                   $sql_insert="INSERT INTO `admin`(`ID`, `account`, `password`) VALUES (NULL,'{$post_username}','{$post_password}')";
+
+                   $result['status']=1;
+                   $result['mag']="已經註冊成功";
+                   $result['lanch']="index.php";
+
+                 }else{                 
+                   $result['status']=1;
+                   $result['mag']="帳號已存在，請重新輸入";
+               }
+               echo json_encode($result);
+
+           }
    }
 
 
